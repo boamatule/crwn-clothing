5148a7048e0e2626e497b4f6ef9cf2fe8ae04f3d
boa <boa.matule@gmail.com>
2021-10-05 19:57:05 +0200

    Creates pages for the sign-in-and-out; Creates  a signInComponent and crrate a basic form. WIP

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import {
   Route
 } from "react-router-dom";
 import './App.css';
-import Header from './components/header/header.component';
+import Header from "./components/header/header.component";
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/signin/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path="/SignIn" component={SignInAndSignUpPage} />
       </Switch>
     </Router>
   </div>
---
+++ src/components/sign-up/sign-up.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./sign-up.styles.scss";
+
+
+const SignUp = () => {
+	return (
+		<div className="">
+
+		</div>
+	);
+}
+
+export default SignUp;
