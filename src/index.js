341da1d53ae9d6a0a2e3b5eb0ebc5217549b4fcd
boa <boa.matule@gmail.com>
2021-10-13 21:51:58 +0200

    Implements Redux PErsistence to take care of the sessionStorage

--- src/index.js
+++ src/index.js
@@ -2,18 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 
 import './index.css';
 import App from './App';
 
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 
 ReactDOM.render(
   <Provider store={store}>
+
     <BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
       <App />
-    </BrowserRouter>
-  </Provider>,
+    </PersistGate>
+  </BrowserRouter>
+   
+  </Provider >,
   document.getElementById('root')
 );
