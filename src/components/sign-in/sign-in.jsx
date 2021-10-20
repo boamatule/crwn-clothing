80fd869f98464bd6ce72aef4b7d9cb110bc45cce
boa <boa.matule@gmail.com>
2021-10-20 20:16:50 +0200

    Converts the scss to styled-components in all the components and pages.

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -1,64 +1,80 @@
 import React from "react";
-import { signInWithGoogle } from "../../firebase/firebase.utils.js";
+
 import CustomButton from "../custom-button/custom-button.component";
 import FormInput from "../form-input/form-input.component";
-import "./sign-in.styles.scss"
 
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
+
+
+
+import {
+  SignInContainer,
+  SignInTitle,
+  ButtonsBarContainer
+} from './sign-in.styles';
 
 class SignIn extends React.Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			email: '',
-			password: ''
-		};
-	}
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
 
-	handleSubmit = event => {
-		event.prevenetDefault();
-		this.seState = ({
-			email: '',
-			password: ''
-		});
-	}
+  handleSubmit = async event => {
+    event.preventDefault();
 
-	handleChange = event => {
-		const { value, name } = event.target;
+    const { email, password } = this.state;
 
-		this.setState({ [name]: value })
-	}
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-	render() {
-		return (
-			<div className="sign-in">
-				<h2 className="title">I already have an account</h2>
-				<span>Sign in with email and password</span>
-				<form onSubmit={this.handleSubmit}>
-					<FormInput
-						name="email" 
-						type="email"
-						value={this.state.email}
-						label="email"
-						handleChange={this.handleChange}
-						required />
-						
-					<FormInput
-						name="password" 
-						type="password"
-						value={this.state.password}
-						label="password"
-						handleChange={this.handleChange}
-						required 
-					/>
-					<div className="buttons">
-						<CustomButton type="submit">SIGN IN</CustomButton>
-						<CustomButton onClick={signInWithGoogle} isGoogleSignIn> Sign in Google</CustomButton>
-					</div>
-				</form>
-			</div>
-		);
-	}
+  handleChange = event => {
+    const { value, name } = event.target;
+
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <SignInContainer>
+        <SignInTitle>I already have an account</SignInTitle>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            name='email'
+            type='email'
+            handleChange={this.handleChange}
+            value={this.state.email}
+            label='email'
+            required
+          />
+          <FormInput
+            name='password'
+            type='password'
+            value={this.state.password}
+            handleChange={this.handleChange}
+            label='password'
+            required
+          />
+          <ButtonsBarContainer>
+            <CustomButton type='submit'> Sign in </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign in with Google
+            </CustomButton>
+          </ButtonsBarContainer>
+        </form>
+      </SignInContainer>
+    );
+  }
 }
 
 export default SignIn;
--- src/components/sign-up/sign-up.jsx
+++ src/components/sign-up/sign-up.jsx
@@ -1,8 +1,11 @@
 import React from "react";
-import { auth, createUserProfileDocument } from "../../firebase/firebase.utils.js";
+
 import CustomButton from "../custom-button/custom-button.component";
 import FormInput from "../form-input/form-input.component";
-import "./sign-up.styles.scss";
+
+import { auth, createUserProfileDocument } from "../../firebase/firebase.utils.js";
+
+import { SignUpContainer, SignUpTitle } from './sign-up.styles';
 
 class SignUp extends React.Component {
   constructor() {
@@ -54,7 +57,7 @@ class SignUp extends React.Component {
   render() {
     const { displayName, email, password, confirmPassword } = this.state;
     return (
-      <div className='sign-up'>
+      <SignUpContainer>
         <h2 className='title'>I do not have a account</h2>
         <span>Sign up with your email and password</span>
         <form className='sign-up-form' onSubmit={this.handleSubmit}>
@@ -92,7 +95,7 @@ class SignUp extends React.Component {
           />
           <CustomButton type='submit'>SIGN UP</CustomButton>
         </form>
-      </div>
+      </SignUpContainer>
     );
   }
 }
