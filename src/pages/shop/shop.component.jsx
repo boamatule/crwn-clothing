e53a63eb79e2b410ffcb78291691c4c527831563
boa <boa.matule@gmail.com>
2021-11-06 19:12:17 +0100

    Creates shopActions and shopTypes and adds the Shop Data to Redux

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,21 +1,30 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
+import { connect } from 'react-redux';
+
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
 
-import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+import { updateCollections } from '../../redux/shop/shop.actions';
+import { 
+  firestore,
+  convertCollectionsSnapshotToMap 
+} from '../../firebase/firebase.utils';
 
 class ShopPage extends React.Component {
   unsubscribeFromSnashop = null;
 
   componentDidMount() {
+    const { updateCollections } = this.props;
     const collectionRef = firestore.collection('collections');
 
-    collectionRef.onSnapshot(async snapshot =>{
-      convertCollectionsSnapshotToMap(snapshot)
-    })
+    this.unsubscribeFromSnashot = collectionRef.onSnapshot(async snapshot => {
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+      updateCollections(collectionsMap);
+    });
   }
+  
   render() {
     const { match } = this.props;
     return (
@@ -27,4 +36,9 @@ class ShopPage extends React.Component {
   }
 }
 
-export default ShopPage;
+const mapDispatchToProps = dispatch => ({
+  updateCollections: collectionsMap => 
+    dispatch(updateCollections(collectionsMap))
+});
+
+export default connect(null, mapDispatchToProps)(ShopPage);
