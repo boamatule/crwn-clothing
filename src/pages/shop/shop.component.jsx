1a7cbfc4d133e41316a99ec308dcc074afc0a027
boa <boa.matule@gmail.com>
2021-11-30 23:42:53 +0100

    install redux-saga package; In the shop folder, creares a shop.sagas file to handle all the saga logic and creates a fetchCollectionsAsync and fetchCollectionsStart generator Functions. WIP

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,18 +2,17 @@ import React from "react";
 import { Route } from "react-router-dom";
 import { connect } from "react-redux";
 
-import { fetchCollectionsStartAsync } from "../../redux/shop/shop.actions";
+import { fetchCollectionsStart } from "../../redux/shop/shop.actions";
 
 import CollectionsOverviewContainer from "../../components/collections-overview/collections-overview.container";
 import CollectionPageContainer from "../collection/collection.container";
 
 class ShopPage extends React.Component {
-  unsubscribeFromSnashop = null;
-
+  // unsubscribeFromSnashot = null;
   componentDidMount() {
-    const { fetchCollectionsStartAsync } = this.props;
+    const { fetchCollectionsStart } = this.props;
 
-    fetchCollectionsStartAsync();
+    fetchCollectionsStart();
   }
 
   render() {
@@ -36,10 +35,10 @@ class ShopPage extends React.Component {
 
 
 const mapDispatchToProps = (dispatch) => ({
-  fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync()),
+  fetchCollectionsStart: () => dispatch(fetchCollectionsStart()),
 });
 
 export default connect(
   null,
-  mapDispatchToProps)
-  (ShopPage);
+  mapDispatchToProps
+  )(ShopPage);
