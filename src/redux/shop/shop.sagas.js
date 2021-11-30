b5a2c4f6b2923c0d437856c56aa74d82a1e8e5e7
boa <boa.matule@gmail.com>
2021-12-01 00:44:57 +0100

    Brings  fetchCollectionsSuccess and fetchCollectionsFailure from shop.actions and creates the Async Generator functions; Creates the root-saga file as the default for the shop.sagas. WIP

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,12 +1,27 @@
-import { takeEvery} from 'redux-saga/effects';
+import { takeLatest, takeEvery, call, put } from 'redux-saga/effects';
+
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+
+import { fetchCollectionsSuccess, fetchCollectionsFailure } from './shop.actions';
 
 import ShopActionsTypes from './shop.types';
 
 export function* fetchCollectionsAsync() {
 	yield console.log('I am fired')
+
+	try {
+		const collectionRef = firestore.collection('collections');
+		const snapshot = yield collectionRef.get();
+		const collectionsMap = yield call(convertCollectionsSnapshotToMap, snapshot);
+		yield put(fetchCollectionsSuccess(collectionsMap));
+	} catch (error) {
+		yield put(fetchCollectionsFailure(error.message))
+	}
 }
 
 export function* fetchCollectionsStart() {
-	yield takeEvery(ShopActionsTypes.FETCH_COLLECTIONS_START,
-	fetchCollectionsAsync)
+	yield takeLatest(
+		ShopActionsTypes.FETCH_COLLECTIONS_START,
+		fetchCollectionsAsync
+	);
 }
