d800da05023a3f151ed662b7a251b4e74bfd8a48
boa <boa.matule@gmail.com>
2021-11-30 21:22:38 +0100

    Creates a CollectionsOverviewContainer and CollectionPageContainer for the ShopePage.

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,30 +1,35 @@
-import ShopActionsTypes from './shop.types';
+import ShopActionTypes from './shop.types';
 
-import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+import {
+  firestore,
+  convertCollectionsSnapshotToMap
+} from '../../firebase/firebase.utils';
 
 export const fetchCollectionsStart = () => ({
-  type: ShopActionsTypes.FETCH_COLLECTIONS_START,
+  type: ShopActionTypes.FETCH_COLLECTIONS_START
 });
 
-export const fetchCollectionsStartSuccess = (collectionsMap) => ({
-  type: ShopActionsTypes.FETCH_COLLECTIONS_SUCCESS,
-  payload: collectionsMap,
+export const fetchCollectionsSuccess = collectionsMap => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
+  payload: collectionsMap
 });
 
-export const fetchCollectionsFailure = (errorMessage) => ({
-  type: ShopActionsTypes.FETCH_COLLECTIONS_FAILURE,
-  payload: errorMessage,
+export const fetchCollectionsFailure = errorMessage => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+  payload: errorMessage
 });
 
-export const fetchCollectionsStartAsync = () => (dispatch) => {
-  const collectionRef = firestore.collection('collections');
-  dispatch(fetchCollectionsStart());
+export const fetchCollectionsStartAsync = () => {
+  return dispatch => {
+    const collectionRef = firestore.collection('collections');
+    dispatch(fetchCollectionsStart());
 
-  collectionRef
-    .get()
-    .then((snapshot) => {
-      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-      dispatch(fetchCollectionsStartSuccess(collectionsMap));
-    })
-    .catch((error) => dispatch(fetchCollectionsFailure(error.Message)));
+    collectionRef
+      .get()
+      .then(snapshot => {
+        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+        dispatch(fetchCollectionsSuccess(collectionsMap));
+      })
+      .catch(error => dispatch(fetchCollectionsFailure(error.message)));
+  };
 };
