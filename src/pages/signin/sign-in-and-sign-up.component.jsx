6824f1cc9c90e0ef4ec96b556fb4c5571bf3f590
boa <boa.matule@gmail.com>
2021-12-01 14:55:54 +0100

    Modifies the sign-in.component to emailSignInStart from the user.actions using sagas; WIP

--- src/pages/signin/sign-in-and-sign-up.component.jsx
+++ src/pages/signin/sign-in-and-sign-up.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import SignIn from '../../components/sign-in/sign-in';
+import SignIn from '../../components/sign-in/sign-in.component';
 import SignUp from '../../components/sign-up/sign-up';
 
 import { SignInAndSignUpContainer } from './sign-in-and-sign-up.styles';
