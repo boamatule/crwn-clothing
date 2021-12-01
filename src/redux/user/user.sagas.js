d4e5388f1c4fb8ead21037cbe8e19b733f453df8
boa <boa.matule@gmail.com>
2021-12-01 16:53:39 +0100

    Creates a reuseble getSnapshotFromUserAuth() for the user.sagas.

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -13,10 +13,9 @@ import {
   createUserProfileDocument,
 } from "../../firebase/firebase.utils";
 
-export function* signInWithGoogle() {
+export function* getSnapshotFromUserAuth(userAuth) {
   try {
-    const user = yield auth.signInWithPopup(googleProvider);
-    const userRef = yield call(createUserProfileDocument, user);
+    const userRef = yield call(createUserProfileDocument, userAuth);
     const userSnapshot = yield userRef.get();
     yield put(
       signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() })
@@ -26,12 +25,18 @@ export function* signInWithGoogle() {
   }
 }
 
+export function* signInWithGoogle() {
+  try {
+    const {user} = yield auth.signInWithPopup(googleProvider);
+    yield getSnapshotFromUserAuth(user);
+  } catch (error) {
+    yield put(signInFailure(error));
+  }
+}
+
 export function* signInWithEmail({ payload: { email, password } }) {
   try {
-    const { user } = yield auth.signInWithEmailAndPassword(email, password);
-		const userRef = yield call(createUserProfileDocument, user);
-    const userSnapshot = yield userRef.get();
-    yield put(signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }));
+    yield getSnapshotFromUserAuth(email, password);
   } catch (error) {
     yield put(signInFailure(error));
   }
