cbe407d9046e8f2c099c5465bcad9e23e7d3f449
boa <boa.matule@gmail.com>
2021-12-02 21:11:52 +0100

    Creates UserActionTypes for the sign up and the userActions. WIP

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -21,16 +21,30 @@ export const emailSignInStart = emailAndPassword => ({
 
 export const checkUserSession = () => ({
 	type: UserActionTypes.CHECK_USER_SESSION
-})
+});
 
 export const signOutStart = () => ({
 	type: UserActionTypes.SIGN_OUT_START
-})
+});
+
 export const signOutSuccess = () => ({
 	type: UserActionTypes.SIGN_OUT_SUCCESS
-})
+});
 
 export const signOutFailure = (error) => ({
 	type: UserActionTypes.SIGN_OUT_FAILURE,
+	payload: error
+});
+
+export const signUpStart = () => ({
+	type: UserActionTypes.SIGN_OUT_START
+})
+
+export const signUpSuccess = () => ({
+	type: UserActionTypes.SIGN_UP_SUCCESS
+})
+
+export const signUpFailure = () => ({
+	type: UserActionTypes.SIGN_UP_FAILURE,
 	payload: error
 })
