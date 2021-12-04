3ce82f027a73e63127d824434de6fb06445d7d44
boa <boa.matule@gmail.com>
2021-12-04 23:46:45 +0100

    Buld the Stripe Payment Route

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import path from 'path';
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
 const app = express();
 const port = procress.env.PORT || 5000
 
@@ -24,4 +26,20 @@ if (process.env.NODE_ENV === 'production') {
 app.listen(port, error => {
 	if (error) throw error;
 	console.log('Server running on port' + port);
+});
+
+app.post('/payment', (req, res) => {
+	const body = {
+		source: req.body.token.id,
+		amount: req.body.amount,
+		currency: 'usd'
+	};
+
+	stripe.charges.create(body, (stripeError, stripeRes) => {
+		if (stripeError) {
+			res.status(500).send({ erorr: stripeError });
+		} else {
+			res.status(200).send({ success: stripeRes });
+		}
+	});
 });
