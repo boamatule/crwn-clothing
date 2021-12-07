225aa21e65f9d8e3a20f28a03c1a92eefcba7fcc
boa <boa.matule@gmail.com>
2021-12-07 14:50:52 +0100

    Separates our cra app into client folder; Added express server to help handle the stripe payments; renames some files.

--- client/src/pages/checkout/checkout.component.jsx
+++ client/src/pages/checkout/checkout.component.jsx
@@ -1,10 +1,11 @@
+import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
-import StripeCheckoutButton from '../../components/stripe-button/stripe-button';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 import { CheckoutPageContainer, CheckoutHeaderContainer, HeaderBlockContainer, TotalContainer, WarningContainer } from './checkout.styles';
 
